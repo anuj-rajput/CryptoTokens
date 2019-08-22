5130e12eccf118c6e975e9c0f551025224337b8a
Anuj Rajput <anujrajput04@gmail.com>
2019-08-22 14:09:33 +0530

    Initialized the project

---
+++ CryptoIcons/Controllers/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  CryptoIcons
+//
+//  Created by Anuj Rajput on 22/08/19.
+//  Copyright © 2019 Matic Network. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CryptoIconsTests/CryptoIconsTests.swift
@@ -0,0 +1,34 @@
+//
+//  CryptoIconsTests.swift
+//  CryptoIconsTests
+//
+//  Created by Anuj Rajput on 22/08/19.
+//  Copyright © 2019 Matic Network. All rights reserved.
+//
+
+import XCTest
+@testable import CryptoIcons
+
+class CryptoIconsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
